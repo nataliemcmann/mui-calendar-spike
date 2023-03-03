7b97aa8d9f0389ac021bcff8ec58593b8a71c584
Natalie McMann <nmcmann110@gmail.com>
2023-03-03 15:31:11 -0600

    moved check in and check out to components

---
+++ src/DatePickers/DatePickers.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useState } from 'react';
+//import mui components
+import { TextField, Button } from '@mui/material';
+//import dateAdapter for Luxon
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+//import datetime form luxon
+import { DateTime } from 'luxon';
+//MUI X components
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+
+function DatePickers() {
+    const currentDate = DateTime.now();
+    const minStayDate = currentDate.plus({months: 1});
+
+    const [checkIn, setCheckIn] = useState(currentDate);
+    const [checkOut, setCheckOut] = useState(minStayDate);
+
+    // function resetCheckOut(newValue){
+    //     console.log('add a month to check out automatically');
+    //     setCheckOut(newValue.plus({months: 1}))
+    // }
+
+    function sendDates() {
+        let dateObject = {
+            checkIn,
+            checkIn
+        };
+        console.log(dateObject);
+    }
+
+    return(
+        <>
+            <div className="checkInDate">
+                <LocalizationProvider dateAdapter = { AdapterLuxon }>
+                <DatePicker
+                    mask="__/__/____"
+                    label="Check In Date"
+                    value={checkIn}
+                    onChange={(newValue) => {
+                    setCheckIn(newValue);
+                    // resetCheckOut(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                </LocalizationProvider>
+                </div>
+                <div className="checkOutDate">
+                <LocalizationProvider dateAdapter = { AdapterLuxon }>
+                <DatePicker
+                    mask="__/__/____"
+                    label="Check Out Date"
+                    value={checkOut}
+                    onChange={(newValue) => {
+                    setCheckOut(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                </LocalizationProvider>
+            </div>
+            <div>
+                <Button onClick={e => {sendDates()}}>Confirm</Button>
+            </div>
+        </>
+    )
+}
+
+export default DatePickers;
